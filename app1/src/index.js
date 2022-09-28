469f86ac7f748cfe45318d4bab00cfde2caf2bf4
rameshdhason <drameshdhas@gmail.com>
2022-09-28 16:21:21 -0400

    First Import

--- app1/src/index.js
+++ app1/src/index.js
@@ -1,5 +1,5 @@
 // You can write your own logic here to determine the actual url
-window.app2Url = "http://localhost:8080"
+window.app2Url = "http://localhost:8081"
 
 // Use dynamic import here to allow webpack to interface with module federation code
 import("./bootstrap");
