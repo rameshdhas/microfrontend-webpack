6434350210c3a5a2bcd97fc0f63f7c06d2379760
rameshdhason <drameshdhas@gmail.com>
2022-09-28 18:09:30 -0400

    First Import

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,4 +1,6 @@
 import React, {Suspense} from "react";
+import ErrorBoundary from "./error-boundary"
+
 const RemoteApp = React.lazy(() => import("button/Button"));
 
 const App = () => {
@@ -12,9 +14,15 @@ const App = () => {
       }}>
         <h1>App1</h1>
       </div>
+      <ErrorBoundary
+            fallback={
+              <div>Stop trying to make fetch happen. It's not gonna happen.</div>
+            }
+      >
       <Suspense fallback={"loading..."}>
         <RemoteApp/>
       </Suspense>
+      </ErrorBoundary>
     </div>)
 }
 
--- app1/src/index.js
+++ app1/src/index.js
@@ -2,4 +2,5 @@
 window.app2Url = "http://localhost:8081"
 
 // Use dynamic import here to allow webpack to interface with module federation code
-import("./bootstrap");
+//import("./bootstrap");
+import('./bootstrap').catch(error => console.log('The dependency is down'));
